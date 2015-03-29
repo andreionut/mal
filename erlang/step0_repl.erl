e799578db493f6b880cf0a2fc992f0fbbeffdc3c
Andrei Balcanasu <abalcanasu@zendesk.com>
2015-03-29 12:38:36 +0100

    Erlang step0 refactor

--- erlang/step0_repl.erl
+++ erlang/step0_repl.erl
@@ -4,10 +4,19 @@
 
 main(_) ->
   io:fwrite("~s", ["user> "]),
-  print(eval(read())),
+  print(eval(read(read_line()))),
   main([]).
 
-read() ->
+read(Input) ->
+  Input.
+
+eval(Input) ->
+  Input.
+
+print(Input) ->
+  io:fwrite("~s", [Input]).
+
+read_line() ->
   case file:read_line(standard_io) of
     {ok, Input} -> Input;
     eof -> exit(eof);
@@ -15,9 +24,3 @@ read() ->
       io:fwrite("Unexpected error: ~p", [Reason]),
       exit(exit)
   end.
-
-eval(Input) ->
-  Input.
-
-print(Input) ->
-  io:fwrite("~s", [Input]).
